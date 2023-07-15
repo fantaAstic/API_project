2b8fa51045189e96d763a8d0b87bfc28a2970598
Fanta Kebe <fanta.kebe305@gmail.com>
2023-07-15 11:31:53 +0000

    Second commit

---
+++ yt.py
@@ -0,0 +1,20 @@
+import json
+import requests
+from pyyoutube import Api
+
+# Initialize the API instance with your API key
+api = Api(api_key="AIzaSyBEVx9oQcIobO7yYlU5GxdlEXSWP3qmAIU")
+
+# Send a GET request to retrieve playlists by channel ID
+channel_id = "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+response = api.get_playlists(channel_id=channel_id)
+
+# Access the playlists
+playlists = response.items
+for playlist in playlists:
+    print(playlist.id)
+
+# Print the JSON response
+response_dict = response.to_dict()
+response_json = json.dumps(response_dict, indent=2)
+print(response_json)
